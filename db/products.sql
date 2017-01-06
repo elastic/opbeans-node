3dd371c358d25fce660fef9e18991df27a65f1ab
Vanja Cosic <vanjacosic@gmail.com>
2017-01-06 16:12:27 +0100

    Add a few more products in SQL

--- db/products.sql
+++ db/products.sql
@@ -2,6 +2,9 @@ insert into product_types (id, name) values (1, 'Light Roast Coffee');
 insert into product_types (id, name) values (2, 'Medium Roast Coffee');
 insert into product_types (id, name) values (3, 'Dark Roast Coffee');
 
-insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (1, 'OP-DRC-C1', 'Brazil Verde, Italian Roast', 'Soft, nutty, low acid, with nice bitter-sweet chocolate tastes.', 3, '120', 1500, 3200);
-insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (2, 'OP-MRC-C2', 'Jamaica Blue Mountain, Vienna Roast', 'Rich flavor, rich aroma, moderate acidity, and an even balance.', 2, '60', 20000, 42000);
-insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (3, 'OP-LRC-C3', 'Colombian Supremo, Cinnamon Roast', 'Full bodied with a light acidity making a balanced cup.', 1, '120', 2000, 4200);
+insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (1, 'OP-DRC-C1', 'Brazil Verde, Italian Roast', 'Soft, nutty, low acid, with nice bitter-sweet chocolate tastes.', 3, '80', 1500, 3200);
+insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (2, 'OP-MRC-C2', 'Jamaica Blue Mountain, Vienna Roast', 'Rich flavor, rich aroma, moderate acidity, and an even balance.', 2, '0', 20000, 42000);
+insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (3, 'OP-LRC-C3', 'Colombian Supremo, Cinnamon Roast', 'Full bodied with a light acidity making a balanced cup.', 1, '150', 2000, 4200);
+insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (4, 'OP-LRC-C4', 'Guatemala Antigua, New England Roast', 'Lively acidity, complex spiciness, and chocolate laced aftertaste.', 1, '0', 1300, 2900);
+insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (5, 'OP-MRC-C5', 'Ethiopian Moka Java, Breakfast Roast', 'Possess an intense floral bouquet and create a pleasant cup of coffee.', 2, '130', 1800, 3900);
+insert into products (id, sku, name, description, type_id, stock, cost, selling_price) values (6, 'OP-DRC-C6', 'European Royale, French Roast', 'Begins full, mellows as it lingers and then finishes with a smooth sweet aftertaste.', 3, '70', 1200, 2500);
