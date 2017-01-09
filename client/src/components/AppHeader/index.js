f88c6aff79ba05ca484f87452edb169feacb460f
Vanja Cosic <vanjacosic@gmail.com>
2017-01-09 12:02:18 +0100

    Update header, navigation and styles

--- client/src/components/AppHeader/index.js
+++ client/src/components/AppHeader/index.js
@@ -9,27 +9,28 @@ class AppHeader extends Component {
     return (
         <div className={classnames('AppHeader', className)} {...props}>
             <div className="ui large menu">
+                <div className="ui container">
+                    <Link className="item" activeClassName="active" to="/">
+                        <i className="dashboard icon"></i>
+                        Dashboard
+                    </Link>
 
-                <Link className="item" activeClassName="active" to="/">
-                    <i className="coffee icon"></i>
-                    OpBeans Admin
-                </Link>
+                    <div className="menu right">
+                        <Link className="item" activeClassName="active" to="/products">
+                            <i className="coffee icon"></i>
+                            Products
+                        </Link>
+                        <Link className="item" activeClassName="active" to="/orders">
+                            <i className="cart icon"></i>
+                            Orders
+                        </Link>
+                        <Link className="item" activeClassName="active" to="/customers">
+                            <i className="users icon"></i>
+                            Customers
+                        </Link>
+                    </div>
 
-                <div className="menu right">
-                    <Link className="item" activeClassName="active" to="/products">
-                        <i className="cart icon"></i>
-                        Products
-                    </Link>
-                    <Link className="item" activeClassName="active" to="/orders">
-                        <i className="line chart icon"></i>
-                        Orders
-                    </Link>
-                    <Link className="item" activeClassName="active" to="/customers">
-                        <i className="users icon"></i>
-                        Customers
-                    </Link>
                 </div>
-
             </div>
         </div>
     );
