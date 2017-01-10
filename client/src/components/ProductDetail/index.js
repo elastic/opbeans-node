37e0d1e6da20bcf5cd466ff8f69e2111fd31ee25
Vanja Cosic <vanjacosic@gmail.com>
2017-01-10 14:56:07 +0100

    Impl. product photo in ProductDetail

--- client/src/components/ProductDetail/index.js
+++ client/src/components/ProductDetail/index.js
@@ -12,70 +12,97 @@ const ProductDetail = ({product, productCustomers, getCustomers}) => {
                   <div className="ui text loader">Loading</div>
               </div>
           )}
-          <div className="ui segments">
-              <div className="ui secondary segment">Product overview</div>
-              <div className="ui segment">
-                  <h3 className="ui sub header">
-                      Name
-                  </h3>
-                  <h1 className="">{product.product.name}</h1>
-              </div>
+          <div className="ui middle aligned stackable grid container">
+              <div className="row">
+                  <div className="ten wide column">
+                      <div className="ui segments">
+                          <div className="ui secondary segment">Product overview</div>
+                          <div className="ui segment">
+                              <h3 className="ui sub header">
+                                  Name
+                              </h3>
+                              <h1 className="">{product.product.name}</h1>
+                          </div>
+                          <div className="ui horizontal segments">
+                              <div className="ui segment">
+                                  <h3 className="ui sub header">
+                                      Type
+                                  </h3>
+                                  <Link className='ui blue basic label' to={`/products/${product.product.id}`}>
+                                      <i className="icon tag"></i>
+                                      {product.product.type_name}
+                                  </Link>
+                              </div>
+                              <div className="ui segment">
+                                  <h3 className="ui sub header">
+                                      SKU
+                                  </h3>
+                                  <div className="ui">
+                                      <code>{product.product.sku}</code>
+                                  </div>
+                              </div>
+                          </div>
+                          <div className="ui segment">
+                              <h3 className="ui sub header">
+                                  Description
+                              </h3>
+                              <p className="ui text padded">{product.product.description}</p>
+                          </div>
+                      </div>
+                      <div className="ui segments">
+                          <div className="ui secondary segment">Product status</div>
 
-              <div className="ui horizontal segments">
-                  <div className="ui segment">
-                      <h3 className="ui sub header">
-                          SKU
-                      </h3>
-                      <div className="ui">
-                          <code>{product.product.sku}</code>
+                          <div className="ui horizontal segments">
+                              <div className="ui segment">
+                                  <h3 className="ui sub header">
+                                      Stock
+                                  </h3>
+                                  <span>{product.product.stock}</span>
+                              </div>
+                              <div className="ui segment">
+                                  <h3 className="ui sub header">
+                                      Sold
+                                  </h3>
+                                  <span>{product.product.sold}</span>
+                              </div>
+                          </div>
+
+                          <div className="ui horizontal segments">
+                              <div className="ui segment">
+                                  <h3 className="ui sub header">
+                                      Cost
+                                  </h3>
+                                  <span>{ formatMoney(product.product.cost) }</span>
+                              </div>
+                              <div className="ui segment">
+                                  <h3 className="ui sub header">
+                                      Selling price
+                                  </h3>
+                                  <span>{ formatMoney(product.product.selling_price) }</span>
+                              </div>
+                          </div>
+
                       </div>
                   </div>
-                  <div className="ui segment">
-                      <h3 className="ui sub header">
-                          Type
-                      </h3>
-                      <Link className='ui blue basic label' to={`/products/${product.product.id}`}>
-                          <i className="icon tag"></i>
-                          {product.product.type_name}
-                      </Link>
+                  <div className="six wide column">
+                      <div className="ui segments">
+                          <div className="ui secondary segment">Product photo</div>
+                          <div className="ui segment">
+                              <img src={`/images/products/${product.product.sku}.jpg`} title={product.product.name} />
+                          </div>
+                      </div>
                   </div>
               </div>
-
-              <div className="ui horizontal segments">
-                  <div className="ui segment">
-                      <h3 className="ui sub header">
-                          Stock
-                      </h3>
-                      <span>{product.product.stock}</span>
-                  </div>
-                  <div className="ui segment">
-                      <h3 className="ui sub header">
-                          Cost
-                      </h3>
-                      <span>{ formatMoney(product.product.cost) }</span>
-                  </div>
-                  <div className="ui segment">
-                      <h3 className="ui sub header">
-                          Selling price
-                      </h3>
-                      <span>{ formatMoney(product.product.selling_price) }</span>
+              <div className="row">
+                  <div className="sixteen wide column">
+                      <div className="ui segments ProductCustomersList">
+                          <div className="ui segment clearing secondary">
+                              Customers who bought this product
+                          </div>
+                          <ProductCustomers productCustomers={ productCustomers } getCustomers={ getCustomers }/>
+                      </div>
                   </div>
               </div>
-
-              <div className="ui horizontal segments">
-                  <div className="ui padded segment">
-                      <h3 className="ui sub header">
-                          Description
-                      </h3>
-                      <p className="ui text padded">{product.product.description}</p>
-                  </div>
-              </div>
-          </div>
-          <div className="ui segments ProductCustomersList">
-              <div className="ui segment clearing secondary">
-                  Customers who bought this product
-              </div>
-              <ProductCustomers productCustomers={ productCustomers } getCustomers={ getCustomers }/>
           </div>
       </div>
   );
