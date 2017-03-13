ce965e9540a45cd3aa2be1a2f8c4ba339b615245
Benjamin Wohlwend <benjamin@opbeat.com>
2017-03-13 11:36:20 +0100

    explicitly parse DATABASE_URL

--- server/config.js
+++ server/config.js
@@ -4,13 +4,26 @@ var env = process.env.NODE_ENV || 'development'
 
 if (env === 'development') require('dotenv').config()
 
+if (process.env.DATABASE_URL) {
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  const dbConfig = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+  };
+}
+
 var conf = module.exports = {
   env: env,
   server: {
     port: process.env.PORT || 3001
   },
-  pg: {
-    url: process.env.DATABASE_URL || null,
+  pg: dbConfig || {
     database: process.env.PGDATABASE || 'opbeans'
   },
   redis: process.env.REDIS_URL || null,
