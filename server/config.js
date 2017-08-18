7b655f811e8033c8a39a0bed9b6e6394b7126f69
Thomas Watson <w@tson.dk>
2017-08-18 11:58:33 +0200

    Fix production domain name

--- server/config.js
+++ server/config.js
@@ -23,7 +23,7 @@ switch (env) {
     conf.server.url = 'http://localhost:' + conf.server.port
     break
   case 'production':
-    conf.server.url = 'https://opbeans-elastic.herokuapp.com'
+    conf.server.url = 'https://opbeans.com'
     break
   default:
     throw new Error('Unknown environment: ' + env)
