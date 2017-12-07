9b5f1cec5415f4f3e6d1828f03bcb281d6135a52
Thomas Watson <w@tson.dk>
2017-12-07 12:59:57 +0100

    Allow overwrite of server URL (#10)

--- server/config.js
+++ server/config.js
@@ -7,7 +7,10 @@ if (env === 'development') require('dotenv').config()
 var conf = module.exports = {
   env: env,
   server: {
-    port: process.env.PORT || 3001
+    protocol: process.env.OPBEANS_SERVER_PROTOCOL || 'http:',
+    auth: process.env.OPBEANS_SERVER_AUTH || '',
+    hostname: process.env.OPBEANS_SERVER_HOSTNAME || 'localhost',
+    port: process.env.OPBEANS_SERVER_PORT || process.env.PORT || 3001
   },
   pg: {
     database: process.env.PGDATABASE || 'opbeans'
@@ -18,13 +21,7 @@ var conf = module.exports = {
   }
 }
 
-switch (env) {
-  case 'development':
-    conf.server.url = 'http://localhost:' + conf.server.port
-    break
-  case 'production':
-    conf.server.url = 'http://www.opbeans.com'
-    break
-  default:
-    throw new Error('Unknown environment: ' + env)
-}
+conf.server.url = conf.server.protocol + '//' +
+  (conf.server.auth ? conf.server.auth + '@' : '') +
+  conf.server.hostname +
+  (conf.server.port ? ':' + conf.server.port : '')
