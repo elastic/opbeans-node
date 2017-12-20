6e0fe1da8298deef9f19ae67f671bff6e4672435
Benjamin Wohlwend <beni@elastic.co>
2017-12-20 21:37:25 +0100

    added serviceVersion

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import { init as initApm } from 'elastic-apm-js-base'
 
 let serviceName = process.env.ELASTIC_APM_JS_BASE_SERVICE_NAME;
+let serviceVersion = process.env.ELASTIC_APM_JS_BASE_SERVICE_VERSION || null;
 let serverUrl = process.env.ELASTIC_APM_JS_BASE_SERVER_URL;
 
 initApm({
   serviceName: serviceName,
+  serviceVersion: serviceVersion,
   serverUrl: serverUrl
 })
 
