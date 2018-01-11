3660fec64d407728616c01b626bbc3252e595c42
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2018-01-11 10:31:08 +0100

    feat: generate errors in the page load
    use js-base@0.3.0
    setUserContext
    setCustomContext
    captureError

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,31 @@
 import { init as initApm } from 'elastic-apm-js-base'
 
-let serviceName = process.env.ELASTIC_APM_JS_BASE_SERVICE_NAME;
-let serviceVersion = process.env.ELASTIC_APM_JS_BASE_SERVICE_VERSION || null;
-let serverUrl = process.env.ELASTIC_APM_JS_BASE_SERVER_URL;
+let serviceName = process.env.ELASTIC_APM_JS_BASE_SERVICE_NAME
+let serviceVersion = process.env.ELASTIC_APM_JS_BASE_SERVICE_VERSION || null
+let serverUrl = process.env.ELASTIC_APM_JS_BASE_SERVER_URL
 
-initApm({
+var apm = initApm({
   serviceName: serviceName,
   serviceVersion: serviceVersion,
   serverUrl: serverUrl
 })
+
+apm.setUserContext({
+  ignored: 'is ignored',
+  id: 'id',
+  username: 'username',
+  email: 'user@opbeans.com'
+})
+
+apm.setCustomContext({
+  superUseful: 'info'
+})
+
+try {
+  throw new Error('Test CaptureError')
+} catch (e) {
+  apm.captureError(e)
+}
 
 import React from 'react'
 import ReactDOM from 'react-dom'
