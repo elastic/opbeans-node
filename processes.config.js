f1931061c1e4c0c267f1d19bbcb072caef2691bb
Stephen Belanger <stephen.belanger@elastic.co>
2018-09-20 15:25:17 -0700

    fix: appease the linter gods

--- processes.config.js
+++ processes.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   apps: [{
-    name: "workload",
-    script: "./node_modules/.bin/workload",
-    args: "-f .workload.js",
+    name: 'workload',
+    script: './node_modules/.bin/workload',
+    args: '-f .workload.js',
     instances: 1,
     restart_delay: 2000
   }, {
-    name: "server",
-    script: "./server.js",
+    name: 'server',
+    script: './server.js',
     instances: 1
   }, {
-    name: "api",
-    script: "./api.js",
+    name: 'api',
+    script: './api.js',
     instances: 1
   }]
-};
+}
