a3e2cd1baba80f56a4f995892361a46ce1791286
Stephen Belanger <stephen.belanger@elastic.co>
2018-09-24 09:41:57 -0700

    chore: split ecosystem.config.js

--- ecosystem-workload.config.js
+++ ecosystem-workload.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   apps: [{
+    name: 'workload',
+    script: './node_modules/.bin/workload',
+    args: '-f .workload.js',
+    instances: 1,
+    restart_delay: 2000
+  }, {
     name: 'server',
     script: './server.js',
     instances: 1
