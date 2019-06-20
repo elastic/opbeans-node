eb0f7da3a163390bfc089548560bff2d2caed29e
Thomas Watson <w@tson.dk>
2019-06-20 19:14:15 +0200

    fix: allow workload script to be disabled
    The apm-integration-testing project will set the `WORKLOAD_DISABLED`
    environment variable based on the `--no-opbeans-node-loadgen` command
    line flag.

--- ecosystem-workload.config.js
+++ ecosystem-workload.config.js
@@ -1,13 +1,21 @@
-module.exports = {
+'use strict'
+
+const config = {
   apps: [{
-    name: 'workload',
-    script: './node_modules/.bin/workload',
-    args: '-f .workload.js',
-    instances: 1,
-    restart_delay: 2000
-  }, {
     name: 'server',
     script: './server.js',
     instances: 1
   }]
 }
+
+if (process.env.WORKLOAD_DISABLED !== 'True') {
+  config.apps.push({
+    name: 'workload',
+    script: './node_modules/.bin/workload',
+    args: '-f .workload.js',
+    instances: 1,
+    restart_delay: 2000
+  })
+}
+
+module.exports = config
