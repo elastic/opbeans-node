b98fa2aa511a7358674ef7e497c5eb9fabc37981
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-11-28 13:52:12 +0000

    ci(jenkins): script to bump the given version

---
+++ .ci/bump-version.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+AGENT_VERSION="${1?Missing the APM Node.js agent version}"
+
+## Use docker to bump the version to ensure the environment is easy to reproduce.
+docker run --rm -t \
+  --user $UID \
+  -e HOME=/tmp \
+  -e AGENT_VERSION="${AGENT_VERSION}" \
+  -w /app \
+  -v "$(pwd):/app" \
+  node:12-alpine /bin/sh -c "set -x
+    CI=true npm install elastic-apm-node@${AGENT_VERSION}"
+
+# Commit changes
+git add package.json
+git commit -m "Bump version ${AGENT_VERSION}"
