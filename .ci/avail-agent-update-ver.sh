606d8528eeaee2e82cc407654694089165b8b8ed
Trent Mick <trent.mick@elastic.co>
2022-06-08 16:24:16 -0700

    try this (expect the fail with bogus agent ver)

---
+++ .ci/avail-agent-update-ver.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Print the version of the latest elastic-apm-node (e.g. "3.36.0") if it is a
+# version this repo hasn't yet updated to. Otherwise, print nothing.
+
+set -euo pipefail
+
+# Use docker to ensure the environment is easy to reproduce, and doesn't rely
+# on 'with${FOO}Env' directives in the calling Jenkinsfile code.
+docker run --rm -t \
+  -e HOME=/tmp \
+  -w /app \
+  -v "$(pwd):/app" \
+  node:14-alpine /bin/sh -c '
+    CURR=$(node -e "console.log(require(\"./package-lock.json\").dependencies[\"elastic-apm-node\"].version)")
+    LATEST=$(npm info elastic-apm-node@latest version)
+    if [[ "$CURR" != "$LATEST" ]]; then
+      echo $LATEST
+    fi
+  '
