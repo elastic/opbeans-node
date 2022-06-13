be6c6ea1dd6f598d50d04849da7249d48a44fd9e
Trent Mick <trent.mick@elastic.co>
2022-06-13 16:21:02 -0700

    hack for testing; going to use a separate top-level pipeline and keep it all in this repo

--- .ci/avail-agent-update-ver.sh
+++ .ci/avail-agent-update-ver.sh
@@ -5,6 +5,9 @@
 
 set -euo pipefail
 
+echo "1.2.3"
+exit
+
 # Use docker to ensure the environment is easy to reproduce, and doesn't rely
 # on 'with${FOO}Env' directives in the calling Jenkinsfile code.
 docker run --rm -t \
