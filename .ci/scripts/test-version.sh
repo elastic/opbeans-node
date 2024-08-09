47c0d9505bddf251d4d26387b1f31e7227b9782c
Victor Martinez <VictorMartinezRubio@gmail.com>
2024-08-09 09:44:22 +0200

    udpatecli: fix if version is already installed

--- .ci/scripts/test-version.sh
+++ .ci/scripts/test-version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-test $(jq -r '.dependencies."elastic-apm-node".version' package-lock.json) != $1
+test $(jq -r '.packages."node_modules/elastic-apm-node".version' package-lock.json) != $1
 
 exit $?
